4e0f66d0f805264bafbfe6e242aecc9ba8014ca1
Victor Costan <costan@gmail.com>
2014-11-08 09:05:13 -0500

    One more bugfix.

--- script/build-ios.sh
+++ script/build-ios.sh
@@ -43,9 +43,9 @@ cp "tmp/ios/platforms/android/ant-build/CordovaApp-debug.apk" \
 cp "tmp/ios/platforms/android/ant-build/CordovaApp-release.apk" \
     "bin/$APP_NAME-cordova-release.apk" || true
 rm -rf "bin/$APP_NAME.app"
-cp -r "tmp/ios/platforms/ios/build/emulator/$APP_NAME.app" bin/ || true
 rm -rf "bin/$APP_NAME-debug.app"
-mv "bin/$APP_NAME.app" "bin/$APP_NAME-debug.app"
-cp -r "tmp/ios/platforms/ios/build/device/$APP_NAME.app" bin/ || true
 rm -rf "bin/$APP_NAME-release.app"
+mv tmp/ios/platforms/ios/build/emulator/*.app "bin/$APP_NAME.app" || true
+mv "bin/$APP_NAME.app" "bin/$APP_NAME-debug.app"
+mv "tmp/ios/platforms/ios/build/device/$APP_NAME.app" "bin/$APP_NAME.app" || true
 mv "bin/$APP_NAME.app" "bin/$APP_NAME-release.app" || true
